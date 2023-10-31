a4eee30395c9b153d19815aded89d23f8028084c
Urvish Prajapati <urvishprajapati412@gmail.com>
2023-10-31 00:32:17 -0400

    tinyapp updated

--- express_server.js
+++ express_server.js
@@ -1,6 +1,13 @@
 const express = require("express");
+let ejs = require('ejs');
+
 const app = express();
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+
 const PORT = 8080; // default port 8080
+
+let generateRandomString = function() {};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -17,6 +24,25 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+
+app.get("/urls/new", (req, res) => {
+  res.render('urls_new');
+  // ejs.render('urls_new');
+});
+
+app.get("/u/:id", (req, res) => {
+  const longURL = 'urls/id';
+  res.redirect(longURL);
+});
+
+// -------------- POST REQ -------------------
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  urlDatabase['id'] = req.body.longURL;
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
