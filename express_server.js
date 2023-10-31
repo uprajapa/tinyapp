484e6b17c336e3803833515688d715bd43bbe8c1
Urvish Prajapati <urvishprajapati412@gmail.com>
2023-10-31 18:36:45 -0400

    username displayed

--- express_server.js
+++ express_server.js
@@ -1,9 +1,10 @@
 const express = require("express");
-let ejs = require('ejs');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 const PORT = 8080; // default port 8080
 
@@ -15,6 +16,7 @@ const urlDatabase = {
 };
 
 app.get("/", (req, res) => {
+  // console.log(`Signed cookies: ${req.signedCookies}`);
   res.send("Hello!");
 });
 
@@ -28,16 +30,32 @@ app.get("/hello", (req, res) => {
 
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    urls: urlDatabase,
+  };
+  if (req.cookies['username']) {
+    templateVars['username'] = req.cookies['username'];
+  }
   res.render('urls_index', templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render('urls_new');
+  const templateVars = {
+  };
+  if (req.cookies['username']) {
+    templateVars['username'] = req.cookies['username'];
+  }
+  res.render('urls_new', templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id]
+  };
+  if (req.cookies['username']) {
+    templateVars['username'] = req.cookies['username'];
+  }
   res.render("urls_show", templateVars);
 });
 
